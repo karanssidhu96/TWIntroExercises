d22f6bf9d43846e8ce70be59d2c1695eb08f207d
karanssidhu96 <karanssidhu96@gmail.com>
2018-08-24 16:14:47 +0100

    Created method for drawing an isosceles triangle

--- src/DiamondExercises.java
+++ src/DiamondExercises.java
@@ -12,7 +12,7 @@ public class DiamondExercises {
         System.out.print(ASTERISK);
     }
 
-    private int calcualteNumberOfSpaces(int n)
+    private int calculateNumberOfSpaces(int n)
     {
         return 2*n-2;
     }
@@ -20,5 +20,40 @@ public class DiamondExercises {
     private int calculateNumberOfAsterisks(int n)
     {
         return 2*n - 1;
+    }
+
+    public void drawAnIsoscelesTriangle(int n)
+    {
+        int numberOfStars;
+        int numberOfSpaces = calculateNumberOfSpaces(n);
+        int spacesSplit;
+
+        for (int i=1; i<=n; i++)
+        {
+            numberOfStars = calculateNumberOfAsterisks(i);
+            if (numberOfSpaces == 1)
+            {
+                spacesSplit = 0;
+            }
+            else
+            {
+                spacesSplit = numberOfSpaces/2;
+            }
+
+            for (int j=0; j<spacesSplit; j++)
+            {
+                printSpace();
+            }
+            for (int j=0; j<numberOfStars; j++)
+            {
+                printAsterisk();
+            }
+            for (int j=0; j<spacesSplit; j++)
+            {
+                printSpace();
+            }
+            numberOfSpaces = numberOfSpaces - 2;
+            System.out.print("\n");
+        }
     }
 }
